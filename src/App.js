21baa4e71992110d69a465fa3796617601333c6c
alperen <alperenyln1@gmail.com>
2023-11-03 18:37:20 +0300

    commit yeni degisiklik

--- src/App.js
+++ src/App.js
@@ -2,30 +2,20 @@ import React, { useState } from "react";
 import { Transition } from "react-transition-group";
 import "./App.css";
 import BackgroundPage from "./Components/BackgroundPage";
-import HamburgerButton from "./Components/HamburgerButton";
 import Sidenav from "./Components/Sidenav";
 import { useAmazonContext } from "./Context/AmazonContext";
 
 function App() {
-  const [navOpen, setNavOpen] = useState(false);
+  const [navOpen, setNavOpen] = useState(true); // Navbar'ı başlangıçta açık olarak ayarlayın
   const { setSubContainer } = useAmazonContext();
-
-  const openNav = () => {
-    setNavOpen(true);
-  };
-
-  const closeNav = () => {
-    setNavOpen(false);
-  };
 
   const { entryStore } = useAmazonContext();
 
   return (
     <div className="App">
-      <HamburgerButton click={openNav} />
       <Transition
         in={navOpen && entryStore}
-        timeout={300}
+        timeout={0}
         mountOnEnter
         unmountOnExit
       >
@@ -34,7 +24,7 @@ function App() {
           return (
             <>
               <Sidenav state={state} />
-              <div
+              {/* <div
                 className="overlay"
                 style={
                   state === "entering"
@@ -43,9 +33,8 @@ function App() {
                     ? { opacity: "1" }
                     : { animation: "show .3s backwards reverse" }
                 }
-                onClick={closeNav}
-              ></div>
-              <div
+              ></div> */}
+              {/* <div
                 className="closeBtn"
                 style={
                   state === "entering"
@@ -54,10 +43,9 @@ function App() {
                     ? { opacity: "1" }
                     : { animation: "show .3s backwards reverse" }
                 }
-                onClick={closeNav}
               >
                 &times;
-              </div>
+              </div> */}
             </>
           );
         }}
